cb53f5b2d241d291fab3e1696b4548fd1b751a9d
Marcos Salgueiro Balsa <marcos.salgueiro@gmail.com>
2019-10-15 22:21:20 +0200

    t:O((R-L)*logn), s:O(1)

---
+++ prime-number-of-set-bits-in-binary-representation/Solution.scala
@@ -0,0 +1,31 @@
+object Solution {
+  def countPrimeSetBits(L: Int, R: Int): Int = {
+    val primes = sieveTo(32)
+    var count = 0
+    for (n <- L to R) {
+      if (primes(bitsSet(n))) count += 1
+    }
+    count
+  }
+  private [this] def sieveTo(n: Int): Array[Boolean] = {
+    val primes = Array.fill(n+1)(true)
+    primes(0) = false
+    primes(1) = false
+    for (i <- primes.indices.drop(2).takeWhile(_ < math.sqrt(n)); if primes(i))
+      for (j <- i * 2 to n by i) {
+        primes(j) = false
+      }
+    primes
+  }
+  private [this] def bitsSet(n: Int): Int = {
+    var _n = n
+    var count = 0
+    while (_n > 0) {
+      if ((_n & 1) == 1) {
+        count += 1
+      }
+      _n >>= 1
+    }
+    count
+  }
+}
