09ca218b10f6a46f0a7a5f5f4774fb4996fe1c06
Marcos Salgueiro Balsa <marcos.salgueiro@gmail.com>
2020-02-15 21:31:12 +0100

    Small correction

--- minimum_number_of_steps_to_make_two_strings_anagram/Solution.java
+++ minimum_number_of_steps_to_make_two_strings_anagram/Solution.java
@@ -4,27 +4,25 @@ import java.util.HashMap;
 import java.util.Map;
 
 class Solution {
-    class Solution {
-        public int minSteps(String s, String t) {
-            int steps = 0;
-            final var sMap = counts(s);
-            final var tMap = counts(t);
-            for (var sCharCount : sMap.entrySet()) {
-                var diff = sCharCount.getValue() - tMap.getOrDefault(sCharCount.getKey(), 0);
-                if (diff > 0) steps += diff;
-            }
-            return steps;
+    public int minSteps(String s, String t) {
+        int steps = 0;
+        final var sMap = counts(s);
+        final var tMap = counts(t);
+        for (var sCharCount : sMap.entrySet()) {
+            var diff = sCharCount.getValue() - tMap.getOrDefault(sCharCount.getKey(), 0);
+            if (diff > 0) steps += diff;
         }
-        private Map<Character,Integer> counts(String s) {
-            final var map = new HashMap<Character,Integer>();
-            for (int i = 0; i < s.length(); i++) {
-                if (!map.containsKey(s.charAt(i))) {
-                    map.put(s.charAt(i), 0);
-                }
-                map.put(s.charAt(i),map.get(s.charAt(i))+1);
+        return steps;
+    }
+    private Map<Character,Integer> counts(String s) {
+        final var map = new HashMap<Character,Integer>();
+        for (int i = 0; i < s.length(); i++) {
+            if (!map.containsKey(s.charAt(i))) {
+                map.put(s.charAt(i), 0);
             }
-            return map;
+            map.put(s.charAt(i),map.get(s.charAt(i))+1);
         }
+        return map;
     }
 }
 
