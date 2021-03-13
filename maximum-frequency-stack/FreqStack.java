c4c48b9f25859a651f2933568f0e2fec0d3b3708
Marcos Salgueiro Balsa <marcos.salgueiro@gmail.com>
2021-03-13 21:06:44 +0100

    Update FreqStack.java

--- maximum-frequency-stack/FreqStack.java
+++ maximum-frequency-stack/FreqStack.java
@@ -24,10 +24,3 @@ class FreqStack {
         return val;
     }
 }
-
-/**
- * Your FreqStack object will be instantiated and called as such:
- * FreqStack obj = new FreqStack();
- * obj.push(val);
- * int param_2 = obj.pop();
- */
