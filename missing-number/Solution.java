128a7eb14055761877041d097d02301d61707b88
Marcos Salgueiro Balsa <marcos.salgueiro@gmail.com>
2021-03-17 19:49:26 +0100

    t: O(n), s: O(1)

---
+++ missing-number/Solution.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+        int xor = nums.length;
+        for (int i = 0; i < nums.length; i++) {
+            xor ^= nums[i];
+            xor ^= i;
+        }
+            
+        return xor;
+    }
+}
