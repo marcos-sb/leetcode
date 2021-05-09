a6359113dcf36652e10194ab0329a5abf4a6a04c
Marcos Salgueiro Balsa <marcos.salgueiro@gmail.com>
2021-05-09 17:43:29 +0200

    t: O(1), s: O(1)

--- reverse-bits/Solution.java
+++ reverse-bits/Solution.java
@@ -1,14 +1,11 @@
 public class Solution {
     public long reverseBits(long n) {
         long res = 0;
-        int k = 32;
-        while (true) {
-            res |= n & 1;
-            if (--k == 0) break;
+        for (int i = 0; i < 32; i++) {
             res <<= 1;
+            res |= n & 1;
             n >>>= 1;
         }
-
         return res;
     }
 }
