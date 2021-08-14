ec4126ec945f1b288bccacfaa4aebc865cceff26
Marcos Salgueiro Balsa <marcos.salgueiro@gmail.com>
2021-08-14 20:07:41 +0200

    t: O(1), s: O(n)

---
+++ range-sum-immutable/Solution.java
@@ -0,0 +1,13 @@
+class NumArray {
+    private final int[] sum;
+    
+    public NumArray(int[] nums) {
+        sum = new int[nums.length+1];
+        for (int i = 0; i < nums.length; i++)
+            sum[i+1] = sum[i] + nums[i];
+    }
+    
+    public int sumRange(int left, int right) {
+        return sum[right+1] - sum[left];
+    }
+}
