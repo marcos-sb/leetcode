cefb739ffa2f3a66259af4e1d11cd46b85b7784e
Marcos Salgueiro Balsa <marcos.salgueiro@gmail.com>
2021-09-05 20:36:38 +0200

    t: O(n), s: O(h)
    h: balanced(tree) ? log n : n

---
+++ lowest-common-ancestor/Solution2.java
@@ -0,0 +1,13 @@
+class Solution2 {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null) return null;
+        if (root.val == p.val) return p;
+        if (root.val == q.val) return q;
+        final var left = lowestCommonAncestor(root.left, p, q);
+        final var right = lowestCommonAncestor(root.right, p, q);
+        if (left != null && right != null) return root;
+        if (left == null && right == null) return null;
+        if (left != null) return left;
+        return right;
+    }
+}
