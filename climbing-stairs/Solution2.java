d2322bf1716aae5d602e4d87dc38cf4a15384153
Marcos Salgueiro Balsa <marcos.salgueiro@gmail.com>
2021-12-20 16:41:23 +0000

    t: O(n), s: O(n)
    Bottom-up DP

---
+++ climbing-stairs/Solution2.java
@@ -0,0 +1,11 @@
+class Solution2 {
+    public int climbStairs(int n) {
+        final var dp = new int[n+1];
+        dp[0] = 1; dp[1] = 1;
+        
+        for (var i = 2; i <= n; i++)
+            dp[i] = dp[i-1] + dp[i-2];
+        
+        return dp[n];
+    }
+}
