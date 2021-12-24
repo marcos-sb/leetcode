793cc01c1cb62b3cc27f0258c956ba45a73d5f8e
Marcos Salgueiro Balsa <marcos.salgueiro@gmail.com>
2021-12-24 13:07:13 +0000

    t: O(n), s: O(h)
    Recursive
    h: log n (balanced) or n

---
+++ symmetric-tree/Solution2.java
@@ -0,0 +1,12 @@
+class Solution2 {
+    public boolean isSymmetric(TreeNode root) {
+        return isSymmetric(root, root);
+    }
+    
+    private static boolean isSymmetric(TreeNode left, TreeNode right) {
+        if (left == null && right == null) return true;
+        if (left == null || right == null) return false;
+        if (left.val != right.val) return false;
+        return isSymmetric(left.left, right.right) && isSymmetric(left.right, right.left);
+    }
+}
