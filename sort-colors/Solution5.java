6a9a059bdcd78fbe623a1f76f7f273d194ff17d8
Marcos Salgueiro Balsa <marcos.salgueiro@gmail.com>
2022-05-27 22:34:41 +0100

    t: O(n), t: O(1)
    With a small change in line 13, we ensure we visit each position only once

---
+++ sort-colors/Solution5.java
@@ -0,0 +1,27 @@
+class Solution {
+    public void sortColors(int[] nums) {
+        var N = nums.length;
+        var i = 0;
+        var k = 0;
+        var j = N-1;
+        
+        while (i < j) {
+            while (i < j && nums[i] == 0) i++;
+            while (i < j && nums[j] == 2) j--;
+            if (i == j) break;
+            if (nums[i] == nums[j]) { // 1 == 1
+                if (k < i) k = i+1;
+                while (k < j && nums[k] == 1) k++;
+                if (k == j) break;
+                if (nums[k] == 0) swap(i, k, nums);
+                else swap(j, k, nums);
+            } else swap(i, j, nums);
+        }
+    }
+    
+    private static void swap(int i, int j, int[] nums) {
+        final var tmp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = tmp;
+    }
+}
