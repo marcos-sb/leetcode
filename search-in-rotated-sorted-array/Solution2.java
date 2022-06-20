719e3e2a7533b4dcddc0944998bd23ce55e6dffa
Marcos Salgueiro Balsa <marcos.salgueiro@gmail.com>
2022-06-20 20:08:30 +0100

    t: O(logN), s: O(1)

---
+++ search-in-rotated-sorted-array/Solution2.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        if (nums.length == 0) return -1;
+        
+        final var N = nums.length;
+        var lo = 0;
+        var hi = N-1;
+        while (lo <= hi) {
+            final var mid = (hi-lo)/2 + lo;
+            if (target == nums[mid]) return mid;
+            if (nums[lo] <= nums[mid]) {
+                if (nums[lo] <= target && target < nums[mid]) hi = mid-1;
+                else lo = mid+1;
+            } else {
+                if (nums[mid] < target && target <= nums[hi]) lo = mid+1;
+                else hi = mid-1;
+            }
+        }
+        
+        return -1;
+    }
+}
