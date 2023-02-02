525b89b5c0df168ff7103295613153e342cb447f
Marcos Salgueiro Balsa <marcos.salgueiro@gmail.com>
2023-02-02 21:12:21 +0000

    t: O(min(l1,l2)), s: O(1)

---
+++ merge-two-sorted-lists/Solution2.java
@@ -0,0 +1,19 @@
+class Solution {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        final var dummyHead = new ListNode();
+        var tail = dummyHead;
+        while (list1 != null && list2 != null) {
+            if (list1.val < list2.val) {
+                tail.next = list1;
+                list1 = list1.next;
+            } else {
+                tail.next = list2;
+                list2 = list2.next;
+            }
+            tail = tail.next;
+        }
+        tail.next = list1 != null ? list1 : list2;
+        
+        return dummyHead.next;
+    }
+}
