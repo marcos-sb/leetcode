2c70461bb10317d266ff6291692f48e070a499b6
Marcos Salgueiro Balsa <marcos.salgueiro@gmail.com>
2023-02-20 21:46:55 +0000

    Small addtions to the documentation

--- task-scheduler/Solution2.java
+++ task-scheduler/Solution2.java
@@ -12,14 +12,15 @@ public class Solution {
         for (int i = 0; i < freqs.length; i++)
             if (freqs[i] == maxFreq) maxFreqTasks++;
         
-        // the last interval doesn't need idle time
+        // the last interval doesn't need idle time so we don't count it here
         final int fullIntervalLen = (maxFreq - 1) * (n+1);
         
         // once the full intervals have been filled with tasks,
-        // the tasks that remain are [1] 1 task each of those with maxFreq, and
-        // [2] any other task that didn't fit in the full intervals, if any
-        final int res = fullIntervalLen + maxFreqTasks;
-        if (res < tasks.length) return tasks.length;
+        // the tasks that remain are:
+        // [1] 1 task each of those with maxFreq (because we didn't count the last interval),
+        // [2] any other task that didn't fit in the full intervals, if any.
+        final int res = fullIntervalLen + maxFreqTasks; // [1]
+        if (res < tasks.length) return tasks.length;    // [2]
         return res;
     }
 }
