6a692b70a932ec29f1fde8de2c1cf6152e1a3e15
Marcos Salgueiro Balsa <marcos.salgueiro@gmail.com>
2023-03-15 00:19:23 +0000

    t: O(N*N), s: O(1)

---
+++ rotate-image/Solution3.java
@@ -0,0 +1,16 @@
+class Solution {
+    public void rotate(int[][] matrix) {
+        final var N = matrix.length;
+        for (var j = 0; j < N/2; j++) {
+            for (var i = j; i < N-1-j; i++) {
+                var tmp = matrix[i][N-1-j];
+                matrix[i][N-1-j] = matrix[j][i];
+                var tmp2 = matrix[N-1-j][N-1-i];
+                matrix[N-1-j][N-1-i] = tmp;
+                tmp = matrix[N-1-i][j];
+                matrix[N-1-i][j] = tmp2;
+                matrix[j][i] = tmp;
+            }
+        }
+    }
+}
