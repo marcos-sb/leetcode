ab8458ebcadc27937c3f35c894448130458faa7e
Marcos Salgueiro Balsa <marcos.salgueiro@gmail.com>
2023-08-23 10:34:17 +0100

    t: O(N), s: O(1)

---
+++ middle-of-the-linked-list/Solution3.java
@@ -0,0 +1,11 @@
+public class MiddleNode{
+    public static LinkedListNode middleNode(LinkedListNode head) {
+        var slow = head;
+        var fast = head;
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        return slow;
+    }
+}
