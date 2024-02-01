632408a398e17f43ae7206baeba8f0e3b9c80d26
Marcos Salgueiro Balsa <marcos.salgueiro@gmail.com>
2024-02-01 23:13:33 +0000

    t: O(N), s: O(h), h: height of tree

---
+++ lowest-common-ancestor-of-a-binary-tree/Solution2.java
@@ -0,0 +1,10 @@
+class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null) return null;
+        if (root == p || root == q) return root;
+        final var left = lowestCommonAncestor(root.left, p, q);
+        final var right = lowestCommonAncestor(root.right, p, q);
+        if (left != null && right != null) return root;
+        return left != null ? left : right;
+    }
+}
