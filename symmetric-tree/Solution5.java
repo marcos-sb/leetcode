df1259c0ccc58867e792361a19bd446748e241c8
Marcos Salgueiro Balsa <marcos.salgueiro@gmail.com>
2024-02-16 21:51:14 +0000

    t: O(N), s: O(h), h: height of tree

---
+++ symmetric-tree/Solution5.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean isSymmetric(TreeNode root) {
+        return isSymmetric(root.left, root.right);
+    }
+
+    private static boolean isSymmetric(TreeNode left, TreeNode right) {
+        if (left == null && right == null) return true;
+        if (left == null || right == null) return false;
+        if (left.val != right.val) return false;
+        return isSymmetric(left.left, right.right) && isSymmetric(left.right, right.left);
+    }
+}
