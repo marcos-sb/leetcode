8e797211382229525b4c24bdf0787d763e515295
Marcos Salgueiro Balsa <marcos.salgueiro@gmail.com>
2025-02-15 19:01:00 +0000

    t: O(N*M), s: O(M)

--- longest-common-prefix/Solution4.java
+++ longest-common-prefix/Solution4.java
@@ -1,10 +1,10 @@
 class Solution {
+
     public String longestCommonPrefix(String[] strs) {
-        final var N = strs.length;
         var prefix = strs[0];
         for (var word : strs) {
             while (!word.startsWith(prefix)) {
-                prefix = prefix.substring(0, prefix.length()-1);
+                prefix = prefix.substring(0, prefix.length() - 1);
             }
         }
         return prefix;
