e3740c1b2e8faaf4c100c0e45fe2970207b5abb6
Marcos Salgueiro Balsa <marcos.salgueiro@gmail.com>
2025-03-13 20:36:04 +0000

    t: O(N), s: O(logN)

---
+++ invert-binary-tree/Solution4.java
@@ -0,0 +1,11 @@
+class Solution {
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null) return null;
+        final var tmp = root.left;
+        root.left = root.right;
+        root.right = tmp;
+        invertTree(root.left);
+        invertTree(root.right);
+        return root;
+    }
+}
