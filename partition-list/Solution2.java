dbce97fe58234d9adae65b49d90e1799dbc9e97b
Marcos Salgueiro Balsa <marcos.salgueiro@gmail.com>
2025-05-26 12:03:39 +0100

    time: O(n), space: O(1)

---
+++ partition-list/Solution2.java
@@ -0,0 +1,35 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode partition(ListNode head, int x) {
+        final var beforeHead = new ListNode();
+        final var afterHead = new ListNode();
+
+        var before = beforeHead;
+        var after = afterHead;
+
+        while (head != null) {
+            if (head.val < x) {
+                before.next = head;
+                before = before.next;
+            } else {
+                after.next = head;
+                after = after.next;
+            }
+            head = head.next;
+        }
+
+        before.next = afterHead.next;
+        after.next = null;
+
+        return beforeHead.next;
+    }
+}
