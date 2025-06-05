208b96d871cb279fa6f7bb7c2fc1803f294a343b
Marcos Salgueiro Balsa <marcos.salgueiro@gmail.com>
2025-06-05 21:20:25 +0100

    time: O(n^2*log(n)), space: O(log(n))

---
+++ construct-quad-tree/Solution.java
@@ -0,0 +1,76 @@
+/*
+// Definition for a QuadTree node.
+class Node {
+    public boolean val;
+    public boolean isLeaf;
+    public Node topLeft;
+    public Node topRight;
+    public Node bottomLeft;
+    public Node bottomRight;
+
+
+    public Node() {
+        this.val = false;
+        this.isLeaf = false;
+        this.topLeft = null;
+        this.topRight = null;
+        this.bottomLeft = null;
+        this.bottomRight = null;
+    }
+
+    public Node(boolean val, boolean isLeaf) {
+        this.val = val;
+        this.isLeaf = isLeaf;
+        this.topLeft = null;
+        this.topRight = null;
+        this.bottomLeft = null;
+        this.bottomRight = null;
+    }
+
+    public Node(boolean val, boolean isLeaf, Node topLeft, Node topRight, Node bottomLeft, Node bottomRight) {
+        this.val = val;
+        this.isLeaf = isLeaf;
+        this.topLeft = topLeft;
+        this.topRight = topRight;
+        this.bottomLeft = bottomLeft;
+        this.bottomRight = bottomRight;
+    }
+}
+*/
+
+class Solution {
+    public Node construct(int[][] grid) {
+        return construct(grid, 0, 0, grid.length);
+    }
+
+    private static Node construct(int[][] grid, int row, int col, int size) {
+        if (size == 1) {
+            return new Node(grid[row][col] == 1, true);
+        }
+
+        if (allSame(grid, row, col, size)) {
+            return new Node(grid[row][col] == 1, true);
+        }
+
+        final var halfSize = size / 2;
+
+        final var topLeft = construct(grid, row, col, halfSize);
+        final var topRight = construct(grid, row, col+halfSize, halfSize);
+        final var bottomLeft = construct(grid, row+halfSize, col, halfSize);
+        final var bottomRight = construct(grid, row+halfSize, col+halfSize, halfSize);
+
+        return new Node(false, false, topLeft, topRight, bottomLeft, bottomRight);
+    }
+
+    private static boolean allSame(int[][] grid, int row, int col, int size) {
+        final var value = grid[row][col];
+        for (var i = row; i < row+size; ++i) {
+            for (var j = col; j < col+size; ++j) {
+                if (value != grid[i][j]) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
