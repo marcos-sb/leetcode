6ead8a0588f02f543fd16ac9fa5cbcc43121017a
Marcos Salgueiro Balsa <marcos.salgueiro@gmail.com>
2025-07-20 12:58:33 +0100

    time: O(m+n), space: O(1)

---
+++ merge-sorted-array/Solution5.java
@@ -0,0 +1,16 @@
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        var i = m-1;
+        var j = n-1;
+        for (var k = m+n-1; 0 <= k; --k) {
+            if (i < 0)
+                nums1[k] = nums2[j--];
+            else if (j < 0)
+                nums1[k] = nums1[i--];
+            else if (nums1[i] < nums2[j])
+                nums1[k] = nums2[j--];
+            else
+                nums1[k] = nums1[i--];
+        }
+    }
+}
